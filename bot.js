44cb2e5238feee22bee9c35270717f27c89b4c4f
botttts <45695807+botttts@users.noreply.github.com>
2018-12-07 20:57:45 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'هنا') {
+    msg.reply('ماستر الملك الكبير');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'هاي') {
+    msg.reply('هلا فيك حب');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
+
